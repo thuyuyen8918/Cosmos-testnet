86db4eb1b9bf871ddfeede0535b10574e008fa6c
thuyuyen8918 <109055532+thuyuyen8918@users.noreply.github.com>
2022-09-28 00:43:50 +0700

    Update alerting.sh

--- Utility/alerting.sh
+++ Utility/alerting.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 
 # File name for saving parameters, e.g. "cosmos.log"
-LOG_FILE="$HOME/alerts/nodealerts.log"
+LOG_FILE="$HOME/.alerting/alerting.log"
+
 # Your node RPC address, e.g. "http://127.0.0.1:26657"
-NODE_RPC="http://127.0.0.1:26657"
-source
-# Trusted node RPC address, e.g. "https://rpc.cosmos.network:26657"
-SIDE_RPC="https://haqq-rpc.onepiece-cosmos-explorer.xyz"
+NODE_RPC="http://127.0.0.1:30657"
+
+# YOUR node name
+NODE_NAME="HAQQ"
+
+# YOUR email
+EMAIL="abcxyz@gmail.com"
+
+# source
+
+# Public trusted node RPC address
+PUBLIC_TRUSTED_RPC="https://haqq-rpc.gei-explorer.xyz"
+
+# Your public IP
 ip=$(wget -qO- eth0.me)
 
 touch $LOG_FILE
-REAL_BLOCK=$(curl -s "$SIDE_RPC/status" | jq '.result.sync_info.latest_block_height' | xargs )
+
+REAL_BLOCK=$(curl -s "$PUBLIC_TRUSTED_RPC/status" | jq '.result.sync_info.latest_block_height' | xargs )
 STATUS=$(curl -s "$NODE_RPC/status")
 CATCHING_UP=$(echo $STATUS | jq '.result.sync_info.catching_up')
 LATEST_BLOCK=$(echo $STATUS | jq '.result.sync_info.latest_block_height' | xargs )
 VOTING_POWER=$(echo $STATUS | jq '.result.validator_info.voting_power' | xargs )
 ADDRESS=$(echo $STATUS | jq '.result.validator_info.address' | xargs )
-source $LOG_FILE
-#REAL_BLOCK=350000
-#VOTING_POWER=150
 
+source $LOG_FILE
 echo 'LAST_BLOCK="'"$LATEST_BLOCK"'"' > $LOG_FILE
 echo 'LAST_POWER="'"$VOTING_POWER"'"' >> $LOG_FILE
 
-source $HOME/.bash_profile
+
+# source $HOME/.bash_profile
 curl -s "$NODE_RPC/status"> /dev/null
 if [[ $? -ne 0 ]]; then
-    MSG="$ip node is stopped!!! ( узел остановлен )"
+    MSG="$ip node is stopped!!!"
     MSG="$NODENAME $MSG"
     SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG"); exit 1
 fi
@@ -36,24 +47,25 @@ if [[ $LAST_POWER -ne $VOTING_POWER ]]; then
     if [[ $DIFF -gt 0 ]]; then
         DIFF="%2B$DIFF"
     fi
-    MSG="$ip voting power changed ( размер стейка валидатора изменился ) $DIFF%0A($LAST_POWER -> $VOTING_POWER)"
+    MSG="$ip voting power changed $DIFF%0A($LAST_POWER -> $VOTING_POWER)"
 fi
 
 if [[ $LAST_BLOCK -ge $LATEST_BLOCK ]]; then
-
-    MSG="$ip node is probably stuck at block (узел застрял в блоке номер>> ) $LATEST_BLOCK"
+    MSG="$ip node is probably stuck at block $LATEST_BLOCK"
 fi
 
 if [[ $VOTING_POWER -lt 1 ]]; then
-    MSG="$ip validator inactive\jailed ( валидатор не активен\в тюрьме ). Voting power $VOTING_POWER"
+    MSG="$ip validator inactive\jailed . Voting power $VOTING_POWER"
 fi
 
 if [[ $CATCHING_UP = "true" ]]; then
-    MSG="$ip node is unsync, catching up ( узел находится в процессе синхронизации ). $LATEST_BLOCK -> $REAL_BLOCK"
+    MSG="$ip node is unsync, catching up. $LATEST_BLOCK -> $REAL_BLOCK"
 fi
 
 if [[ $REAL_BLOCK -eq 0 ]]; then
-    MSG="$ip can't connect to ( пропало соединения с рпц нодой ) $SIDE_RPC"
+    MSG="$ip can't connect to $PUBLIC_TRUSTED_RPC"
+    sendmail $EMAIL <<< "Subject: $MSG"
+    
 fi
 
 if [[ $MSG != "" ]]; then
