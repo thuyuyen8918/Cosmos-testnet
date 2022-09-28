d411a05b0924e8e7b278ee474ac475b9f6a2f052
thuyuyen8918 <109055532+thuyuyen8918@users.noreply.github.com>
2022-09-28 16:50:13 +0700

    Update alerting.sh

--- Utility/alerting.sh
+++ Utility/alerting.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
-# File name for saving parameters, e.g. "cosmos.log"
+# File name for saving parameters
 LOG_FILE="$HOME/.alerting/alerting.log"
 
 # Your node RPC address, e.g. "http://127.0.0.1:26657"
-NODE_RPC="http://127.0.0.1:30657"
+NODE_RPC="http://127.0.0.1:26657"
 
 # YOUR node name
-NODE_NAME="HAQQ"
+NODE_NAME="HAQQ-Test"
 
 # YOUR email
-EMAIL="abcxyz@gmail.com"
+EMAIL="thuyuyen8918@gmail.com"
 
 source
 
 # Public trusted node RPC address
-PUBLIC_TRUSTED_RPC="https://haqq-rpc.gei-explorer.xyz"
+# PUBLIC_TRUSTED_RPC="https://haqq-rpc.gei-explorer.xyz"
+PUBLIC_TRUSTED_RPC="http://94.130.239.162:26657"
 
 # Your public IP
 ip=$(wget -qO- eth0.me)
 
 touch $LOG_FILE
 
-REAL_BLOCK=$(curl -s "$PUBLIC_TRUSTED_RPC/status" | jq '.result.sync_info.latest_block_height' | xargs )
+# Collect status of node
+REAL_BLOCK=$(curl -s "$PUBLIC_TRUSTED_RPC/status" --connect-timeout 20 | jq '.result.sync_info.latest_block_height' | xargs )
 STATUS=$(curl -s "$NODE_RPC/status")
 CATCHING_UP=$(echo $STATUS | jq '.result.sync_info.catching_up')
 LATEST_BLOCK=$(echo $STATUS | jq '.result.sync_info.latest_block_height' | xargs )
 VOTING_POWER=$(echo $STATUS | jq '.result.validator_info.voting_power' | xargs )
 ADDRESS=$(echo $STATUS | jq '.result.validator_info.address' | xargs )
+
+# Collect node version
+NODE_VERSION=$(curl -s "$NODE_RPC/abci_info" | jq .result.response.version | tr -d \\ | tr -d '"')
+TRUSTED_RPC_VERSION=$(curl -s "$PUBLIC_TRUSTED_RPC//abci_info" --connect-timeout 20 | jq .result.response.version | tr -d \\ | tr -d '"')
 
 source $LOG_FILE
 echo 'LAST_BLOCK="'"$LATEST_BLOCK"'"' > $LOG_FILE
@@ -37,9 +43,15 @@ echo 'LAST_POWER="'"$VOTING_POWER"'"' >> $LOG_FILE
 source $HOME/.bash_profile
 curl -s "$NODE_RPC/status"> /dev/null
 if [[ $? -ne 0 ]]; then
-    MSG="$ip node is stopped!!!"
-    MSG="$NODENAME $MSG"
+    MSG="Node $NODE_NAME with $ip is stopped!!!"
+    sendmail $EMAIL <<< "Subject: $MSG"
     SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG"); exit 1
+fi
+
+if [ "$NODE_VERSION" != "$TRUSTED_RPC_VERSION" ]; then
+    MSG="Node $NODE_NAME with $ip is running wrong version $NODE_VERSION. Correct version is $TRUSTED_RPC_VERSION!!!"
+    sendmail $EMAIL <<< "Subject: $MSG"
+    SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG");
 fi
 
 if [[ $LAST_POWER -ne $VOTING_POWER ]]; then
@@ -47,28 +59,32 @@ if [[ $LAST_POWER -ne $VOTING_POWER ]]; then
     if [[ $DIFF -gt 0 ]]; then
         DIFF="%2B$DIFF"
     fi
-    MSG="$ip voting power changed $DIFF%0A($LAST_POWER -> $VOTING_POWER)"
+    MSG="Node $NODE_NAME with $ip has changed voting power: $DIFF%0A($LAST_POWER -> $VOTING_POWER)"
+    sendmail $EMAIL <<< "Subject: $MSG"
+    SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG");
 fi
 
 if [[ $LAST_BLOCK -ge $LATEST_BLOCK ]]; then
-    MSG="$ip node is probably stuck at block $LATEST_BLOCK"
+    MSG="Node $NODE_NAME with $ip got probably stuck at block $LATEST_BLOCK"
+    sendmail $EMAIL <<< "Subject: $MSG"
+    SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG");
 fi
 
 if [[ $VOTING_POWER -lt 1 ]]; then
-    MSG="$ip validator inactive\jailed . Voting power $VOTING_POWER"
+    MSG="Node $NODE_NAME with $ip is inactive\jailed. Voting power $VOTING_POWER"
+    sendmail $EMAIL <<< "Subject: $MSG"
+    SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG");
 fi
 
 if [[ $CATCHING_UP = "true" ]]; then
-    MSG="$ip node is unsync, catching up. $LATEST_BLOCK -> $REAL_BLOCK"
+    MSG=" Node $NODE_NAME with $ip is not full synched, catching up. $LATEST_BLOCK -> $REAL_BLOCK"
+    sendmail $EMAIL <<< "Subject: $MSG"
+    SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG");
 fi
 
 if [[ $REAL_BLOCK -eq 0 ]]; then
-    MSG="$ip can't connect to $PUBLIC_TRUSTED_RPC"
+    MSG="Can't connect to $PUBLIC_TRUSTED_RPC"
     sendmail $EMAIL <<< "Subject: $MSG"
-    
-fi
+    SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG");
 
-if [[ $MSG != "" ]]; then
-    MSG="$NODENAME $MSG"
-    SEND=$(curl -s -X POST -H "Content-Type:multipart/form-data" "https://api.telegram.org/bot$TG_API/sendMessage?chat_id=$TG_ID&text=$MSG")
 fi
